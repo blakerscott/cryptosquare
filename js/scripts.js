4de6b0d7f647d773858d475bb6e5b1f69d5f0c0a
blake scott and gracie lundell <blakerscott@gmail.com>
2016-01-19 14:08:12 -0800

    plan out how to get the working sentence

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,60 @@
-var cryptoSquare = function(letter) {
-  if (letter === " ") {
-    return true;
-  } else if(letter.upperCase) {
-    return true;
-  } else {
-    return true;
-  }
 
 
-};
+//take user input
+//convert user input to string
+//remove spaces
+//count characters
+//remove punctuation
+//ASSIGN THE RESULT TO var sentence
+
+
+//this is assigning the var sentence to an example user imput in order to complete buisness logic first
+var sentence = 
+
+
+var cryptoSquare = function(sentence) {
+  return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var cryptoSquare = function(letter) {
+//   if (letter === " ") {
+//     return true;
+//   } else if(letter.upperCase) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// };
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,40 @@
 describe('cryptoSquare', function() {
   it("is true when there is input", function() {
-    expect(cryptoSquare(NaN)).to.equal(true);
-  });
-
-  it("is true when the code will eliminate spaces", function() {
-    expect(cryptoSquare(NaN)).to.equal(true);
-  });
-
-  it("is false when letters are returned as uppercase", function() {
-    expect(cryptoSquare(NaN)).to.equal(true);
+    expect(cryptoSquare(sentence)).to.equal(true);
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe('cryptoSquare', function() {
+//   it("is true when there is input", function() {
+//     expect(cryptoSquare(NaN)).to.equal(true);
+//   });
+//
+//   it("is true when the code will eliminate spaces", function() {
+//     expect(cryptoSquare(NaN)).to.equal(true);
+//   });
+//
+// });
